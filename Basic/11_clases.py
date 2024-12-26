86e2676f8e8d111f880d1405f31f24046147529a
duquediazn <duquediazn@gmail.com>
2024-12-26 15:14:58 +0000

    Clases

--- Basic/11_clases.py
+++ Basic/11_clases.py
@@ -22,6 +22,8 @@ class Person:
 # Los protegidos se prefijan con _ (convención, no es estrictamente privado).
 # Los privados se prefijan con __ (esto aplica 'name mangling' para evitar colisiones de nombres).
     def __init__(self, name, surname, alias="Sin alias"): # Constructor
+        #self.name = name
+        #self.surname = surname
         self.full_name = f"{name} {surname} ({alias})"  # Propiedad pública, 'self' es el equivalente a 'this'
         self.__name = name  # Propiedad privada
 # Los métodos se definen como funciones dentro de una clase y siempre reciben self como primer parámetro.
@@ -33,8 +35,10 @@ class Person:
 
 
 my_person = Person("Brais", "Moure")
+#print(my_person.name)
 print(my_person.full_name)
 print(my_person.get_name())
+
 my_person.walk()
 
 my_other_person = Person("Brais", "Moure", "MoureDev")

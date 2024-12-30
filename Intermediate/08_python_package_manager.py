c9e50e0e97536499bd6831a707a0ee8cc167f0fc
duquediazn <duquediazn@gmail.com>
2024-12-30 17:15:17 +0000

    PIP

--- Intermediate/08_python_package_manager.py
+++ Intermediate/08_python_package_manager.py
@@ -2,36 +2,41 @@
 
 # PIP https://pypi.org
 
-# pip install pip
-# pip --version
+# Instalación de PIP y paquetes desde consola: 
 
-# pip install numpy
-""" import pandas
-from mypackage import arithmetics
-import requests """
-import numpy
+# pip install pip # Windows:  py -m pip install
+# pip --version # Windows: py -m pip --version
+
+# pip install numpy # Windows: py -m pip install numpy
+
+# pip install pandas # Windows: py -m pip install pandas
+
+# pip list # Windows: py -m pip lsit
+# pip uninstall pandas # Windows: py -m pip uninstall pandas
+# pip show numpy # Windows: py -m pip show numpy
+
+# pip install requests # Windows: py -m pip install requests
+
+import pandas #https://pandas.pydata.org/
+#from mypackage import arithmetics
+import requests
+import numpy #https://numpy.org/
 
 print(numpy.version.version)
 
-""" numpy_array = numpy.array([35, 24, 62, 52, 30, 30, 17])
-print(type(numpy_array))
+numpy_array = numpy.array([35, 24, 62, 52, 30, 30, 17])
+print(type(numpy_array)) # <class 'numpy.ndarray'>
 
-print(numpy_array * 2)
+print(numpy_array * 2) # [ 70  48 124 104  60  60  34]
 
-# pip install pandas
 
-# pip list
-# pip uninstall pandas
-# pip show numpy
-
-# pip install requests
 
 response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=151")
-print(response)
-print(response.status_code)
+print(response) # <Response [200]>
+print(response.status_code) # 200
 print(response.json())
 
 # Arithmetics Package
 
 
-print(arithmetics.sum_two_values(1, 4)) """
+#print(arithmetics.sum_two_values(1, 4))
